b49f9827b3fc6eea53c3b76a1ecf200ab96579c0
Kimyeonho <kyh0703@nate.com>
2023-11-27 17:22:21 +0900

    [Feautre] NVIM -> Lazyvim으로 변환

---
+++ .config/nvim/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.cmd("packadd! dracula_pro")
+vim.cmd("syntax enable")
+vim.cmd("let g:dracula_colorterm = 1")
+-- vim.cmd("colorscheme dracula_pro_blade")
--- .config/nvim/lua/theme.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd [[packadd! dracula_pro]]
-vim.cmd [[syntax enable]]
-vim.cmd [[let g:dracula_colorterm = 0]]
-vim.cmd [[colorscheme dracula_pro]]
